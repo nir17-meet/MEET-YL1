4f0a384839a94e78e8a6651703226d61c0752239
Your Name <you@example.com>
2016-01-06 20:12:54 +0200

    ps

---
+++ exB.py
@@ -0,0 +1,7 @@
+word = str(input("give word"))
+if word[0] == word[2]:
+    print("true")
+else:
+    print("false")
+
+    
---
+++ exericeA.py
@@ -0,0 +1,26 @@
+n = int(input("please enter a number"))
+x = 1
+while(x <= n):
+    print(x)
+    x+=1
+
+print("now ill print only the devide numbers")
+
+x=1
+
+while(x <= n):
+    if(n%x == 0):
+        print(x)
+    x+=1
+
+
+def div():
+    n = input("please enter a number")
+    x=1
+    while(x <= n):
+        if(n%x == 0):
+            print(x)
+    x+=1
+
+
+
